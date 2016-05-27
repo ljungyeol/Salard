659545ddfc77e452d841a286c4c592d53f5fc14f
ljungyeol <ljungyeol@gmail.com>
2016-05-27 18:27:47 +0900

    hi

--- app/src/main/java/com/ghosthawk/salard/MainActivity.java
+++ app/src/main/java/com/ghosthawk/salard/MainActivity.java
@@ -1,20 +1,52 @@
 package com.ghosthawk.salard;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.ghosthawk.salard.Login.LoginActivity;
+import com.ghosthawk.salard.Sell.HomeAdapter;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
-
+    ViewPager viewPager;
+    HomeAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager_home);
+
+        mAdapter = new HomeAdapter(this);
+        viewPager.setAdapter(mAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
         imageView = (ImageView)findViewById(R.id.img_tutorial);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
